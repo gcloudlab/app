19d1089696f3c4c95d09ab98a63229fd153793fa
yesmore <3224266014@qq.com>
2022-07-20 19:49:29 +0800

    feat: 修改一抹多东西

---
+++ src/hooks/usePicture.ts
@@ -0,0 +1,23 @@
+import { getPictureService } from '@/service/api/picture';
+import { ref } from 'vue';
+
+export interface PictureResponse {
+  msg: string;
+  currentPage: string;
+  photoNum: number;
+  totalUsers: number;
+  totalCall: number;
+  uip: string;
+  photoList: string[];
+  info: string;
+}
+const usePicture = async (page: number) => {
+  const picture = ref<PictureResponse>();
+  const res = await getPictureService(page);
+  if (res.status === 200) {
+    picture.value = res.data;
+    // return picture;
+  }
+  return picture.value;
+};
+export default usePicture;
---
+++ src/pages/picture/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="text-center">
+    <n-image-group>
+      <n-space>
+        <div class="flex flex-wrap items-end">
+          <n-image
+            v-for="(item, index) in pictures?.photoList"
+            class="animate__animated animate__fadeIn faster shadow"
+            :key="index"
+            width="250"
+            height="300"
+            :src="item"
+            object-fit="contain"
+            lazy
+            fallback-src="./src/assets/logo.png"
+          >
+            <template #placeholder>
+              <n-skeleton width="200" height="100%" :sharp="false" size="large" />
+            </template>
+          </n-image>
+        </div>
+      </n-space>
+    </n-image-group>
+  </div>
+</template>
+
+<script setup lang="ts">
+import usePicture, { PictureResponse } from '@/hooks/usePicture';
+import { onMounted, ref } from 'vue';
+import { NImageGroup, NImage, NSpace, NSkeleton } from 'naive-ui';
+
+const pictures = ref<PictureResponse>();
+onMounted(async () => {
+  pictures.value = await usePicture(8);
+});
+</script>
+
+<style lang="scss" scoped></style>
