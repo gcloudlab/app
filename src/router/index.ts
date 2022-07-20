7d1e20d09567a2a48bf57520658e5069a391a323
yesmore <3224266014@qq.com>
2022-07-20 19:54:19 +0800

    fix: layout not found

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,7 +3,7 @@ import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
-    name: 'Layout',
+    name: 'layout',
     component: () => import('@/pages/layout/index.vue'),
     redirect: '/home',
     children: [
