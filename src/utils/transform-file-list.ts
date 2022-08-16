63557a9177a15a467d5cae69e884724ef747a899
songjunxi <3224266014@qq.com>
2022-08-16 17:23:57 +0800

    upd: 瞎改了一些东西

--- src/utils/transform-file-list.ts
+++ src/utils/transform-file-list.ts
@@ -1,10 +1,10 @@
-import type { FileListData } from '@/models/file';
-import _ from 'lodash';
-import { transformDate } from './date';
+import type { FileListData } from "@/models/file";
+import _ from "lodash";
+import { transformDate } from "./date";
 
 const generateTree = (list: FileListData[] | any, rootId: number) => {
   if (!Array.isArray(list)) {
-    new Error('type only Array');
+    new Error("type only Array");
     return list;
   }
 
@@ -24,42 +24,46 @@ const generateTree = (list: FileListData[] | any, rootId: number) => {
     treeItem.updated_at = transformDate(treeItem.updated_at);
     treeItem.type = fileType(treeItem.ext);
 
-    if (parentId === rootId && item.ext === '') {
+    if (parentId === rootId && item.ext === "") {
       // 已经到根元素则将映射结果放进结果集
       // bug(fixed): 关于此处，为何treeitem放前面，若非如此合并后children为空，猜测是网络原因，因为本地是没问题的
       result.push(_.assign(treeItem, { isFolder: true, children: [] }));
-    } else if (parentId === rootId && item.ext !== '') {
+    } else if (parentId === rootId && item.ext !== "") {
       // 默认文件夹文件
-      other.push({ ...treeItem, type: fileType(treeItem.ext), isFolder: false });
+      other.push({
+        ...treeItem,
+        type: fileType(treeItem.ext),
+        isFolder: false,
+      });
       otherSize += treeItem.size;
     } else {
       // 若父元素不存在，初始化父元素
       if (!objMap[parentId]) {
         objMap[parentId] = { size: 0 };
-        console.log('初始化父元素', parentId);
+        console.log("初始化父元素", parentId);
       }
 
       // 若无该根元素则放入map中
-      if (!objMap[parentId]['children']) {
-        objMap[parentId]['children'] = [];
+      if (!objMap[parentId]["children"]) {
+        objMap[parentId]["children"] = [];
       }
-      objMap[parentId]['children'].push(treeItem);
-      objMap[parentId]['size'] += treeItem.size;
-      objMap[parentId]['isFolder'] = objMap[parentId]?.ext === '';
+      objMap[parentId]["children"].push(treeItem);
+      objMap[parentId]["size"] += treeItem.size;
+      objMap[parentId]["isFolder"] = objMap[parentId]?.ext === "";
     }
   }
   console.log(result);
 
   if (other.length > 0) {
     const otherItem = {
-      name: '默认文件夹',
-      identity: 'other',
+      name: "默认文件夹",
+      identity: "other",
       size: otherSize,
       isFolder: true,
       parent_id: 0,
-      type: '文件夹',
+      type: "文件夹",
       children: other,
-      ext: '',
+      ext: "",
     };
     return [...result, otherItem];
   }
@@ -69,44 +73,44 @@ const generateTree = (list: FileListData[] | any, rootId: number) => {
 
 const fileType = (ext: string) => {
   switch (ext.slice(1).toLowerCase()) {
-    case 'jpg':
-    case 'png':
-    case 'gif':
-    case 'bmp':
-    case 'jpeg':
-      return '图片';
-    case 'doc':
-    case 'docx':
-      return '文档';
-    case 'xls':
-    case 'xlsx':
-      return '表格';
-    case 'ppt':
-    case 'pptx':
-      return '幻灯片';
-    case 'pdf':
-      return 'PDF';
-    case 'zip':
-    case 'rar':
-      return '压缩文件';
-    case 'txt':
-      return '文本文件';
-    case 'mp3':
-    case 'wav':
-      return '音频文件';
-    case 'mp4':
-    case 'avi':
-      return '视频文件';
-    case '':
-      return '文件夹';
+    case "jpg":
+    case "png":
+    case "gif":
+    case "bmp":
+    case "jpeg":
+      return "图片";
+    case "doc":
+    case "docx":
+      return "文档";
+    case "xls":
+    case "xlsx":
+      return "表格";
+    case "ppt":
+    case "pptx":
+      return "幻灯片";
+    case "pdf":
+      return "PDF";
+    case "zip":
+    case "rar":
+      return "压缩文件";
+    case "txt":
+      return "文本文件";
+    case "mp3":
+    case "wav":
+      return "音频文件";
+    case "mp4":
+    case "avi":
+      return "视频文件";
+    case "":
+      return "文件夹";
     default:
       return ext;
   }
 };
 
 export const transformOriginFileList = (list: FileListData[]) => {
-  const folderList = list.filter(item => item.ext === '');
-  return folderList.map(item => {
+  const folderList = list.filter((item) => item.ext === "");
+  return folderList.map((item) => {
     return {
       label: item.name,
       value: item.id,
