30efdd653ad0cc57eb11516134445679e7feb56d
yesmore <3224266014@qq.com>
2022-08-28 21:00:02 +0800

    调整jiojio

--- src/pages/layouts/index.vue
+++ src/pages/layouts/index.vue
@@ -3,25 +3,25 @@
     <Header />
     <n-scrollbar style="max-height: calc(100vh - 60px)">
       <router-view />
+      <Footer />
     </n-scrollbar>
-    <Footer />
   </div>
 </template>
 
 <script setup lang="ts">
-import { onMounted } from "vue";
-import { onBeforeRouteLeave } from "vue-router";
-import { useAuth } from "@/hooks";
-import { useFiles } from "@/hooks/useFiles";
-import Header from "./header.vue";
-import Footer from "./footer.vue";
-import { NScrollbar } from "naive-ui";
+import { onMounted } from 'vue';
+import { onBeforeRouteLeave } from 'vue-router';
+import { useAuth } from '@/hooks';
+import { useFiles } from '@/hooks/useFiles';
+import Header from './header.vue';
+import Footer from './footer.vue';
+import { NScrollbar } from 'naive-ui';
 // const Loading = defineAsyncComponent(() => import('@/components/commons/loading/index.vue'));
 
 const { onGetFileList } = useFiles();
 const { onGetUserDetailAndCheckAuth } = useAuth();
-onBeforeRouteLeave((leaveGuard) => {
-  if (leaveGuard.path !== "/sign") {
+onBeforeRouteLeave(leaveGuard => {
+  if (leaveGuard.path !== '/sign') {
     onGetUserDetailAndCheckAuth();
   }
 });
