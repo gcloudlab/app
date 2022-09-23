52e50ed8a30dd3cf14e36ae13b63d19d1b87839e
yesmore <3224266014@qq.com>
2022-09-23 21:03:50 +0800

    upd: emmm

--- src/pages/sign/index.vue
+++ src/pages/sign/index.vue
@@ -8,18 +8,12 @@
         <div class="auth_options-unregistered">
           <h1 class="auth_unregistered-title font-black tracking-wide">GCloud 云盘</h1>
           <!-- <h2 class="leading-10">让我康康还有谁没注册😰</h2> -->
+          <p class="auth_unregistered-text">GCloud是一款免费的、不限速的在线云盘.</p>
+          <p class="auth_unregistered-text">它的优点有很多，比如巴拉巴拉...</p>
           <p class="auth_unregistered-text">
             tips: 欢迎加入<strong class="italic">Web前端养老院(QQ群)</strong>: 982545311.
           </p>
-          <p class="auth_unregistered-text">
-            tips: 欢迎加入<strong class="italic">Web前端养老院(QQ群)</strong>: 982545311.
-          </p>
-          <p class="auth_unregistered-text">
-            tips: 欢迎加入<strong class="italic">Web前端养老院(QQ群)</strong>: 982545311.
-          </p>
-          <p class="auth_unregistered-text">
-            tips: 欢迎加入<strong class="italic">Web前端养老院(QQ群)</strong>: 982545311.
-          </p>
+
           <n-button type="primary" round class="auth_unregistered-signup" @click="onChangeToSignup">
             <template #icon>
               <n-icon><log-in-icon /></n-icon>
