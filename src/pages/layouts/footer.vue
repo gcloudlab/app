fcc94e2ee51d9ccb1aa9f3da56c77fd88266dd1f
yesmore <3224266014@qq.com>
2022-10-24 18:39:00 +0800

    upd: 更换visitor徽章

--- src/pages/layouts/footer.vue
+++ src/pages/layouts/footer.vue
@@ -1,5 +1,8 @@
 <template>
-  <img style="display: none" src="https://visitor-badge.glitch.me/badge?page_id=gcloud.app" />
+  <!-- <img
+    style="display: none"
+    src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgcloud.website%2Fhit-counter&count_bg=%2379C83D&title_bg=%23555555&icon=awesomelists.svg&icon_color=%2300FF77&title=%E8%AE%BF%E5%AE%A2&edge_flat=true"
+  /> -->
 </template>
 
 <script setup lang="ts"></script>
