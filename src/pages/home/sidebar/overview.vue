31d30ead32eea991f39491f0b79dbd045dba295c
yesmore <3224266014@qq.com>
2022-10-29 13:39:30 +0800

    upd: hide hits

--- src/pages/home/sidebar/overview.vue
+++ src/pages/home/sidebar/overview.vue
@@ -39,6 +39,7 @@
       />
       <div class="text-center mt-3">
         <img
+          style="display: none"
           src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgcloud.website%2Fhit-counter&count_bg=%2379C83D&title_bg=%23555555&icon=awesomelists.svg&icon_color=%2300FF77&title=%E8%AE%BF%E5%AE%A2&edge_flat=true"
         />
       </div>
