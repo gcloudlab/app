901dffbd5a41ae3d1f76bf57023c155c6acf717d
yesmore <3224266014@qq.com>
2022-12-18 16:41:52 +0800

    feat: support capital email

--- src/utils/email.ts
+++ src/utils/email.ts
@@ -1,5 +1,5 @@
 export const validateEmail = (email: string) => {
-  return /^[a-z0-9]+([._\\-]*[a-z0-9])*@([a-z0-9]+[-a-z0-9]*[a-z0-9]+.){1,63}[a-z0-9]+$/.test(
+  return /^[a-zA-Z0-9]+([._\\-]*[a-zA-Z0-9])*@([a-z0-9]+[-a-z0-9]*[a-z0-9]+.){1,63}[a-z0-9]+$/.test(
     email
   );
-}
+};
